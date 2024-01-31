90feea4380f740b5c1b7fd3fe282e6ac8d4a5f83
slava <tfdf63@yandex.ru>
2024-01-31 21:10:02 +0400

    register form

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,10 +1,22 @@
-import * as z from "zod";
+import * as z from 'zod';
 
 export const LoginSchema = z.object({
-    email: z.string().email({
-        message: "Email is required"
-    }),
-    password: z.string().min(1, {
-        message: "Password is required"
-    })
-})
+  email: z.string().email({
+    message: 'Email is required',
+  }),
+  password: z.string().min(1, {
+    message: 'Password is required',
+  }),
+});
+
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: 'Email is required',
+  }),
+  password: z.string().min(6, {
+    message: 'Min 6 characters required',
+  }),
+  name: z.string().min(4, {
+    message: 'Name is required',
+  }),
+});
